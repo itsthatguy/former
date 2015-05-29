7dfe5ae4b84508de38a932fb81e4c7cca47eed5d
Kevin Altman <kevin@itsthatguy.com>
2015-05-29 11:57:39 -0400

    for angular projects, you need to trigger the element or it wont update scope

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-/* global $ */
+/* global $, angular */
 'use strict';
 
 $('body').on('click', '.former-overlay .button', function() {
-  $('[ng-model="formData.first_name"]').val('Bob');
-  $('[ng-model="formData.last_name"]').val('Villa');
-  $('[ng-model="formData.email"], input[type="email"').val('admin@example.com');
-  $('[ng-model="formData.expirationDate"], input[type="tel"').val('5551234567');
-  $('[ng-model="formData.expirationDate"]').val('12/15');
-  $('[ng-model="formData.cardNumber"]').val('4111111111111111');
+
+  var $firstName = $('[ng-model="formData.first_name"]');
+  var $lastName = $('[ng-model="formData.last_name"]');
+  var $email = $('[ng-model="formData.email"], input[type="email"]');
+  var $phone = $('[ng-model="formData.phone"], input[type="tel"]');
+  var $expData = $('[ng-model="formData.expirationDate"]');
+  var $cardNumber = $('[ng-model="formData.cardNumber"]');
+
+  $firstName.val('Bob');
+  $lastName.val('Villa');
+  $email.val('admin@example.com');
+  $phone.val('5551234567');
+  $expData.val('12/15');
+  $cardNumber.val('4111111111111111');
+
+  if (angular) {
+    angular.element($firstName).triggerHandler('input');
+    angular.element($lastName).triggerHandler('input');
+    angular.element($email).triggerHandler('input');
+    angular.element($phone).triggerHandler('input');
+    angular.element($expData).triggerHandler('input');
+    angular.element($cardNumber).triggerHandler('input');
+  }
 });
 
 $('body').append(
